fdbcc97cf2b77a2f07a3d1395fe73bd47efd3a3d
Aalvee Ahtav <aalvee666@gmail.com>
2026-05-02 21:51:48 -0500

    fixed speedometer

--- Assets/Scripts/Minimap.cs
+++ Assets/Scripts/Minimap.cs
@@ -1,19 +1,56 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Minimap : MonoBehaviour
+//{
+
+//    public Transform player;
+
+//    void LateUpdate()
+//    {
+//        Vector3 newPosition = player.position;
+//        newPosition.y = transform.position.y;
+//        transform.position = newPosition;
+
+//        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+//    }
+
+//}
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Minimap : MonoBehaviour
 {
-
     public Transform player;
+
+    void Start()
+    {
+        // Auto-find player if not assigned in Inspector
+        if (player == null)
+        {
+            GameObject playerObj = GameObject.FindWithTag("Player");
+            if (playerObj != null)
+            {
+                player = playerObj.transform;
+            }
+            else
+            {
+                Debug.LogError("Minimap: No player assigned and no GameObject with tag 'Player' found!");
+            }
+        }
+    }
 
     void LateUpdate()
     {
+        // Guard clause prevents the null ref crash
+        if (player == null) return;
+
         Vector3 newPosition = player.position;
         newPosition.y = transform.position.y;
         transform.position = newPosition;
-
         transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
     }
-
 }
